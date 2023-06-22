fc86efe77ac9223c6c8063202993d20de4ea2774
yulu86 <xuyulu86@126.com>
2023-06-22 11:55:22 +0800

    add input

---
+++ learn-python/p2/06_input.py
@@ -0,0 +1,10 @@
+x = input("x: ")
+print(type(x))
+y = int(x) + 1
+print(y)
+print(f"x: {x}, y: {y}")
+
+print(bool(-1))  # True
+print(bool(0))  # False
+print(bool(""))  # False
+print(bool(None))  # False
