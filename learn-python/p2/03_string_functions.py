52c2ffe9888d6f8e8df1d8b732b309a31ed27ee5
yulu86 <xuyulu86@126.com>
2023-06-22 11:02:23 +0800

    update progress

---
+++ learn-python/p2/03_string_functions.py
@@ -0,0 +1,10 @@
+course = "   Python programming   "
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("pro"))
+print(course.replace("o", "j"))
+print("pro" in course)
+print("swift" not in course)
+print(course)
