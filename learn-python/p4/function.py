0216367e01fb44a2e4cab1ca009a64ed10a99357
yulu86 <xuyulu86@126.com>
2023-06-22 17:02:18 +0800

    udf

---
+++ learn-python/p4/function.py
@@ -0,0 +1,34 @@
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+greet("Yulu", "Xu")
+
+
+def get_greeting(name):
+    return f"Hi, {name}"
+
+
+message = get_greeting("John")
+print(message)
+
+
+def increment(number, by=1):
+    return number + by
+
+
+result = increment(2)
+print(result)
+result = increment(2, by=3)
+print(result)
+
+
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+
+print(multiply(2, 3, 4, 5))
