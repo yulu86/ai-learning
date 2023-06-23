af3e4a03f9e80346c2b71461da900ad1136582b4
yulu86 <xuyulu86@126.com>
2023-06-23 11:21:32 +0800

    add list unpack and pack

--- learn-python/p5/02_list_ext1.py
+++ learn-python/p5/02_list_ext1.py
@@ -9,3 +9,26 @@ print(letters[::2])
 numbers = list(range(20))
 print(numbers[::2])
 print(numbers[::-1])
+
+numbers = [1, 2, 3]
+# unpack
+first, second, third = numbers
+print(first)
+print(second)
+print(third)
+
+# unpack, 其中others是packing
+firstNum, secondNum, *others = numbers
+print(firstNum)
+print(secondNum)
+print(others)
+
+def multiply(*numbers):
+    print(numbers)
+
+multiply(1, 2, 3, 4, 5)
+
+
+firstItem, *otherItems, lastItem = numbers
+print(firstItem, lastItem)
+print(otherItems)
