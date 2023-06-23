77ed8fd8b258ec18857f5f95ce6d75d7a3db5964
Yulu Xu <xuyulu86@126.com>
2023-06-23 18:16:17 +0800

    add map function

---
+++ learn-python/p5/06_list_map.py
@@ -0,0 +1,21 @@
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 12),
+]
+
+prices = []
+for item in items:
+    prices.append(item[1])
+
+print(prices)
+
+x = map(lambda item: item[1], items)
+print(x)
+
+
+# for item in x:
+#     print(item)
+
+
+print(list(x))
