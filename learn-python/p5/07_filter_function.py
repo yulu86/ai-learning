b448c864eebece399f71c49183ea9bb54f3c5339
Yulu Xu <xuyulu86@126.com>
2023-06-23 23:12:47 +0800

    add map lambda

--- learn-python/p5/07_filter_function.py
+++ learn-python/p5/07_filter_function.py
@@ -6,3 +6,12 @@ items = [
 
 filteredItems = list(filter(lambda item: item[1] >= 10, items))
 print(filteredItems)
+
+prices = list(map(lambda item:item[1], items))
+print(prices)
+
+prices = [item[1] for item in items]
+print(prices)
+
+prices.sort(reverse=True)
+print(prices)
