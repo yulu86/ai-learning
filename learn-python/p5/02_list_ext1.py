6b7abf0f326dcf43748b7499a01814efba03ea55
yulu86 <xuyulu86@126.com>
2023-06-23 11:01:26 +0800

    add  list demo

---
+++ learn-python/p5/02_list_ext1.py
@@ -0,0 +1,11 @@
+letters = ["a", "b", "c", "d"]
+letters[0] = "A"
+print(letters)
+print(letters[0:3])
+print(letters[:3])
+print(letters[:])
+print(letters[::2])
+
+numbers = list(range(20))
+print(numbers[::2])
+print(numbers[::-1])
