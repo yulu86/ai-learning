8422beae12695509d66a153612b34b46f3342532
Yulu Xu <xuyulu86@126.com>
2023-07-23 15:05:38 +0800

    add properties

---
+++ learn-python/p7/03_properties.py
@@ -0,0 +1,19 @@
+class Product:
+    def __init__(self, price):
+        self.price = price
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price can not be negative")
+        self.__price = value
+
+
+product = Product(10)
+print(product.price)
+
+product.price = -10
