e40d49c7070dd80391f36124e3d303a381d3bf87
Yulu Xu <xuyulu86@126.com>
2023-07-23 15:39:26 +0800

    add inherited

---
+++ learn-python/p7/04_classes_inherited.py
@@ -0,0 +1,23 @@
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+# 继承Animal类
+
+
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+m = Mammal()
+m.eat()
+print(m.age)
