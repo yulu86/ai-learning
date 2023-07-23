1d0e945e274c924229af9245cd4020368056eeb6
Yulu Xu <xuyulu86@126.com>
2023-07-23 14:31:34 +0800

    add add magic method

--- learn-python/p7/01_class.py
+++ learn-python/p7/01_class.py
@@ -11,6 +11,9 @@ class Point:
     def __gt__(self, other):
         return self.x > other.x and self.y > other.y
 
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
     def __str__(self):
         return f"Point ({self.x}, {self.y})"
 
@@ -27,5 +30,9 @@ point = Point(3, 4)
 other = Point(1, 2)
 
 print(point)
+print(other)
+
 print(point > other)
 print(point < other)
+
+print(point + other)
