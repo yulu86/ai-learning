6084f94d81a2c5ee7b54cc01ba4dc7521d5003bd
yulu86 <xuyulu86@126.com>
2023-07-30 11:08:14 +0800

    add sub module demo

--- learn-python/p8/app.py
+++ learn-python/p8/app.py
@@ -1,8 +1,11 @@
-from sales import calc_shipping, calc_tax
-import sales
+from ecommerce.shopping import calc_shipping, calc_tax
+from ecommerce.shopping import sales
+import sys
 
 
 calc_shipping()
 calc_tax()
 
 sales.calc_shipping()
+
+print(sys.path)
